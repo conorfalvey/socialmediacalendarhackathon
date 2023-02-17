a4b2c0e061b2b16ea8f50dea2eb11eda3f92eb58
Riley Thompson <rjthompson2@dons.usfca.edu>
2023-02-16 20:08:47 -0800

    adding cli for consumers and broker

--- eda/brokers.py
+++ eda/brokers.py
@@ -5,33 +5,40 @@ class EventBus:
         self.subscribers = {}
         self.queue = []
 
-    def subscribe(self, event_type, subscriber):
+    def subscribe(self, event_type, subscriber) -> None:
         if event_type not in self.subscribers:
             self.subscribers[event_type] = []
         self.subscribers[event_type].append(subscriber)
 
-    def unsubscribe(self, event_type, subscriber):
+    def unsubscribe(self, event_type, subscriber) -> None:
         if event_type not in self.subscribers:
             return
         self.subscribers[event_type].remove(subscriber)
 
     def notify(self, event):
         if str(event.__class__.__name__) not in self.subscribers:
-            print("ended")
             return
         for subscriber in self.subscribers[str(event.__class__.__name__)]:
             # yield asyncio.run(subscriber(event))
             yield subscriber(event)
 
-    def give_event(self, event):
+    def give_event(self, event) -> None:
         self.queue.append(event)
 
-    def listen(self):
+    def listen(self) -> bool:
         while self.queue != []:
             print(self.queue)
             self.notify(self.queue.pop(0))
+            # self.elastic_growth()
         return True
     
 
-    # def create_consumers(self):
-        # 
+    # def elastic_growth(self):
+    #     totals = {}
+    #     for event in self.queue:
+    #         if event not in totals:
+    #             totals[event] = 1
+    #         else:
+    #             totals[event] += 1
+        
+
--- eda/consumers.py
+++ eda/consumers.py
@@ -1,21 +1,47 @@
+from http.server import BaseHTTPRequestHandler
+from urllib.parse import parse_qs, urlparse
+from http.server import HTTPServer
+import json
 
-class Consumer:
-    async def consume(self, event):
-        print("Consumer started")
+class Consumer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Parse URL.
+        parsed_url = urlparse(self.path)
+        route: str = parsed_url.path
+        query: dict = parse_qs(parsed_url.query)  # each entry is a list.
 
-# class ScheduleConsumer(Consumer):
-#     def consume(self, event):
-#         # season_df = await nfl.import_schedules([2022])
-#         season_df = nfl.import_schedules([2022])
-#         season_df = season_df.loc[season_df["gameday"] == str(event.date)]
-#         # season_df = season_df.loc[season_df["gameday"] >= str(event.date)]
-#         season_df = season_df[["game_id", "gameday", "gametime"]]
-#         print(season_df)
-#         return season_df
-#         # ScheduleProducer().produce(season_df)
+        # Response values.
+        r_code: int = 200
+        r_type: str = "application/json"
+        r_content: str = ""
 
-# class RecordConsumer(Consumer):
-#     async def consume(self, event):
-#         #TODO logic
-#         print("TBD")
-#         return
+        try:
+            if route == "/":
+                # Do work
+                print("Working...")
+
+            # Fallback, handle 404s.
+            else:
+                r_code = 404
+                r_content = json.dumps({
+                    "error": "No route found matching {0}".format(route)
+                })
+
+            # Send the response.
+            self.send_response(r_code)
+            self.send_header("Content-Type", r_type)
+            self.end_headers()
+            self.wfile.write(r_content.encode("utf8"))
+
+        # Handle server errors.
+        except Exception as exc:
+            self.send_error(500, message="Server Error.", explain=str(exc))
+
+
+if __name__ == "__main__":
+    try:
+        print("Running web server...")
+        httpd = HTTPServer(("localhost", 8000), Consumer)
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+            print("Bye.")
