905c8c2fc4985882ce7033777638a63054a1c278
Conor Falvey <conor.falvey22@gmail.com>
2023-02-23 22:25:24 -0800

    Hook up user service to controller

--- eda/consumers.py
+++ eda/consumers.py
@@ -1,26 +1,36 @@
 from flask import Flask, request, session
 from events import get_event
+from app.services.user_service import UserService
 
 consumers = Flask(__name__)
 
 
-@consumers.route('/notification', methods=["POST"])
+@consumers.route("/notification", methods=["POST"])
 def notification_consumer():
     return "Success"
 
-@consumers.route('/post', methods=["POST"])
+
+@consumers.route("/post", methods=["POST"])
 def post_consumer():
     form = request.get_json()
     event = get_event(form.pop("Type"))
     event = event(**form)
     return "Success"
 
-@consumers.route('/signup', methods=["POST"])
+
+@consumers.route("/signup", methods=["POST"])
 def sign_up_consumer():
     form = request.get_json()
     event = get_event(form.pop("Type"))
     event = event(**form)
-    print(event.to_repr())
+
+    us = UserService()
+    us.create_user(
+        name=event.name,
+        email=event.email,
+        password=event.password,
+        default_delay=event.default_notification,
+    )
     return "Success"
 
 
